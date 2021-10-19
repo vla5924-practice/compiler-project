49d87841732ce32f4a08352c8e8d5318cc51aa5a
taktaev-artyom <artaktaev@gmail.com>
2021-10-19 22:59:30 +0300

    added test for preprocessor

--- tests/backend/preprocessor.cpp
+++ tests/backend/preprocessor.cpp
@@ -1,5 +1,17 @@
 #include <gtest/gtest.h>
 
+#include "preprocessor/preprocessor.hpp"
+#include "stringvec.hpp"
+
+using namespace preprocessor;
+
 TEST(Test, can_add) {
     ASSERT_EQ(4, 2 + 2);
+}
+
+TEST(Preprocessor, can_remove_comment_for_single_line) {
+    StringVec source = {"x = 1 # x declaration"};
+    StringVec transformed = Preprocessor::process(source);
+    StringVec expected = {"x = 1 "};
+    ASSERT_EQ(expected, transformed);
 }
