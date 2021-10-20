c02757bd1c3fa2a55c3819a1054e74a07f9c3c73
taktaev-artyom <artaktaev@gmail.com>
2021-10-20 17:35:44 +0300

    refactoring

--- backend/src/preprocessor/preprocessor.cpp
+++ backend/src/preprocessor/preprocessor.cpp
@@ -8,26 +8,26 @@ StringVec Preprocessor::removeComments(const StringVec &source) {
         std::string resultStr;
         bool inStringLiteral = false;
         bool inString = false;
-        for (char sym : str) {
+        int i = 0;
+        for (; i < str.length(); i++) {
             // If we are inside the comment entity
             // then we can ignore any symbol until line ends
             // If symbol is quote
             // this means we are entering or leaving string literal
-            if (sym == '\'')
+            if (str[i] == '\'')
                 inStringLiteral = !inStringLiteral;
-            if (sym == '"')
+            if (str[i] == '"')
                 inString = !inString;
             // If we are not inside string literal
             // then we can check if there is '//' sequence
             if (!inStringLiteral && !inString) {
-                if (sym == '#') {
+                if (str[i] == '#') {
                     break;
                 }
             }
-            resultStr.push_back(sym);
         }
-        if (!resultStr.empty())
-            result.push_back(std::move(resultStr));
+        if (i != 0)
+            result.push_back(std::move(str.substr(0, i)));
     }
     return result;
 }
