1b6472f7a4827362aed8aa7d7f5e785ed5ae9625
taktaev-artyom <artaktaev@gmail.com>
2021-10-20 17:42:17 +0300

    refactoring
    rename boolean variables

--- backend/src/preprocessor/preprocessor.cpp
+++ backend/src/preprocessor/preprocessor.cpp
@@ -4,30 +4,24 @@ using namespace preprocessor;
 
 StringVec Preprocessor::removeComments(const StringVec &source) {
     StringVec result;
-    for (auto &str : source) {
+    for (const auto &str : source) {
         std::string resultStr;
-        bool inStringLiteral = false;
-        bool inString = false;
+        bool inStringApostrophe = false;
+        bool inStringQuotes = false;
         int i = 0;
         for (; i < str.length(); i++) {
-            // If we are inside the comment entity
-            // then we can ignore any symbol until line ends
-            // If symbol is quote
-            // this means we are entering or leaving string literal
             if (str[i] == '\'')
-                inStringLiteral = !inStringLiteral;
+                inStringApostrophe = !inStringApostrophe;
             if (str[i] == '"')
-                inString = !inString;
-            // If we are not inside string literal
-            // then we can check if there is '//' sequence
-            if (!inStringLiteral && !inString) {
+                inStringQuotes = !inStringQuotes;
+            if (!inStringApostrophe && !inStringQuotes) {
                 if (str[i] == '#') {
                     break;
                 }
             }
         }
         if (i != 0)
-            result.push_back(std::move(str.substr(0, i)));
+            result.emplace_back(std::move(str.substr(0, i)));
     }
     return result;
 }
