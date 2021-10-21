2c1b8a7a21d10674345cce010f5ef3e2d5207d2f
vla5924 <vla5924@gmail.com>
2021-10-21 18:11:12 +0300

    std::variant in ast::Node
    union is deprecated

--- backend/include/backend/ast/node.hpp
+++ backend/include/backend/ast/node.hpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <memory>
 #include <string>
+#include <variant>
 
 #include "ast/node_type.hpp"
 
@@ -22,6 +23,39 @@ struct Node {
         double fpNumber;
     } numLiteral;
     std::string strLiteral;
+
+    std::variant<long int, double, std::string, size_t> value;
+
+    const long int &intNum() const {
+        return std::get<long int>(value);
+    }
+    const double &fpNum() const {
+        return std::get<double>(value);
+    }
+    const std::string &str() const {
+        return std::get<std::string>(value);
+    }
+    const std::string &id() const {
+        return std::get<std::string>(value);
+    }
+    const size_t &uid() const {
+        return std::get<size_t>(value);
+    }
+    long int &intNum() {
+        return std::get<long int>(value);
+    }
+    double &fpNum() {
+        return std::get<double>(value);
+    }
+    std::string &str() {
+        return std::get<std::string>(value);
+    }
+    std::string &id() {
+        return std::get<std::string>(value);
+    }
+    size_t &uid() {
+        return std::get<size_t>(value);
+    }
 
     Node() = default;
     Node(const NodeType &type_, Ptr parent_ = Ptr()) : type(type_), parent(parent_){};
