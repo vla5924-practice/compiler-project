bb5d69c61fd03840c0e0303475a2a236078531a0
vla5924 <vla5924@gmail.com>
2021-10-21 18:10:12 +0300

    type identification in registry

--- backend/include/backend/parser/type_registry.hpp
+++ backend/include/backend/parser/type_registry.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <unordered_set>
 
 #include "token.hpp"
@@ -7,10 +8,22 @@
 namespace parser {
 
 class TypeRegistry {
-    static std::unordered_set<std::string> userDefinedTypes;
+  public:
+    enum BuiltInTypes : size_t {
+        UnknownType = 0,
+        IntType = 1,
+        FloatType = 2,
+        StrType = 3,
+        NoneType = 4,
+        BuiltInTypesCount,
+    };
+
+  private:
+    static std::map<std::string, size_t> userDefinedTypes;
 
   public:
     static bool isTypename(const Token &token);
+    static size_t typeId(const Token &token);
 };
 
 } // namespace parser
--- backend/src/parser/type_registry.cpp
+++ backend/src/parser/type_registry.cpp
@@ -2,10 +2,27 @@
 
 using namespace parser;
 
-std::unordered_set<std::string> TypeRegistry::userDefinedTypes = {};
+std::map<std::string, size_t> TypeRegistry::userDefinedTypes = {};
 
 bool TypeRegistry::isTypename(const Token &token) {
     return token.is(Token::Keyword::Int) || token.is(Token::Keyword::Float) || token.is(Token::Keyword::String) ||
            token.is(Token::Keyword::None) ||
            (token.type == Token::Type::Identifier && userDefinedTypes.find(token.id()) != userDefinedTypes.end());
+}
+
+size_t TypeRegistry::typeId(const Token &token) {
+    if (token.type == Token::Type::Identifier) {
+        const std::string &id = token.id();
+        auto it = userDefinedTypes.find(id);
+        return it != userDefinedTypes.end() ? it->second : BuiltInTypes::UnknownType;
+    }
+    if (token.is(Token::Keyword::Int))
+        return BuiltInTypes::IntType;
+    if (token.is(Token::Keyword::Float))
+        return BuiltInTypes::FloatType;
+    if (token.is(Token::Keyword::String))
+        return BuiltInTypes::StrType;
+    if (token.is(Token::Keyword::None))
+        return BuiltInTypes::NoneType;
+    return BuiltInTypes::UnknownType;
 }
