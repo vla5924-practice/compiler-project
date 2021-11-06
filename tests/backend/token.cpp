9ad05712694d54f15452b39789d317b4a981d52f
taktaev-artyom <artaktaev@gmail.com>
2021-11-06 21:25:37 +0300

    added ещё tests

--- tests/backend/token.cpp
+++ tests/backend/token.cpp
@@ -8,21 +8,21 @@ TEST(Token, can_construct_keyword) {
     Keyword kw = Keyword::If;
     Token token(kw);
     ASSERT_EQ(token.type, TokenType::Keyword);
-    ASSERT_EQ(token.value, kw);
+    ASSERT_EQ(token.kw(), kw);
 }
 
 TEST(Token, can_construct_special) {
     Special spec = Special::Indentation;
     Token token(spec);
     ASSERT_EQ(token.type, TokenType::Special);
-    ASSERT_EQ(token.value, spec);
+    ASSERT_EQ(token.spec(), spec);
 }
 
 TEST(Token, can_construct_operator) {
     Operator op = Operator::Add;
     Token token(op);
     ASSERT_EQ(token.type, TokenType::Operator);
-    ASSERT_EQ(token.value, op);
+    ASSERT_EQ(token.op(), op);
 }
 
 TEST(Token, can_construct_identifier) {
@@ -30,7 +30,7 @@ TEST(Token, can_construct_identifier) {
     std::string id = "identifier";
     Token token(type, id);
     ASSERT_EQ(token.type, type);
-    ASSERT_EQ(token.value, id);
+    ASSERT_EQ(token.id(), id);
 }
 
 TEST(Token, can_construct_integer_literal) {
@@ -38,7 +38,7 @@ TEST(Token, can_construct_integer_literal) {
     std::string literal = "5924";
     Token token(type, literal);
     ASSERT_EQ(token.type, type);
-    ASSERT_EQ(token.value, literal);
+    ASSERT_EQ(token.literal(), literal);
 }
 
 TEST(Token, can_construct_float_literal) {
@@ -46,7 +46,7 @@ TEST(Token, can_construct_float_literal) {
     std::string literal = "1.0";
     Token token(type, literal);
     ASSERT_EQ(token.type, type);
-    ASSERT_EQ(token.value, literal);
+    ASSERT_EQ(token.literal(), literal);
 }
 
 TEST(Token, can_construct_string_literal) {
@@ -54,5 +54,32 @@ TEST(Token, can_construct_string_literal) {
     std::string literal = "string";
     Token token(type, literal);
     ASSERT_EQ(token.type, type);
-    ASSERT_EQ(token.value, literal);
+    ASSERT_EQ(token.literal(), literal);
+}
+
+TEST(Token, can_compare_equal) {
+    Token token1(Keyword::Float);
+    Token token2(Keyword::Float);
+    ASSERT_TRUE(token1 == token2);
+}
+
+TEST(Token, can_compare_not_equal) {
+    Token token1(Keyword::Float);
+    Token token2(Keyword::Int);
+    ASSERT_TRUE(token1 != token2);
+}
+
+TEST(Token, can_check_is_keyword) {
+    Token token(Keyword::Float);
+    ASSERT_TRUE(token.is(Keyword::Float));
+}
+
+TEST(Token, can_check_is_operator) {
+    Token token(Operator::Add);
+    ASSERT_TRUE(token.is(Operator::Add));
+}
+
+TEST(Token, can_check_is_special) {
+    Token token(Special::EndOfExpression);
+    ASSERT_TRUE(token.is(Special::EndOfExpression));
 }
