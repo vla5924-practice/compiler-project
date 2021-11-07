8ce099c35f53ffb30d8d60dde990f55803a35126
MaksimShagov <makcim.shagov@mail.ru>
2021-11-07 14:51:06 +0300

    style fix

--- backend/include/backend/base_error.hpp
+++ backend/include/backend/base_error.hpp
@@ -5,7 +5,8 @@
 
 class BaseError : public std::exception {
     std::string what_str;
+
   public:
-    BaseError(size_t line_number, size_t column_number, const std::string& message);
+    BaseError(size_t line_number, size_t column_number, const std::string &message);
     virtual const char *what() const noexcept;
 };
