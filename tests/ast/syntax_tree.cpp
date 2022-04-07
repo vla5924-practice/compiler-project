82d71581341b328760ab7df0da937dd36fecb511
arteboss <43129320+arteboss@users.noreply.github.com>
2022-04-07 14:19:47 +0300

    added syntax tree tests

---
+++ tests/ast/syntax_tree.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include "ast/syntax_tree.hpp"
+
+using namespace ast;
+
+TEST(SyntaxTree, can_compare_same_syntax_tree) {
+    SyntaxTree tree, tree1;
+    tree.root = std::make_shared<Node>(NodeType::ProgramRoot);
+    tree1.root = std::make_shared<Node>(NodeType::ProgramRoot);
+    Node::Ptr child = std::make_shared<Node>(NodeType::FunctionDefinition, tree.root);
+    tree.root->children.push_back(child);
+    Node::Ptr child1 = std::make_shared<Node>(NodeType::FunctionDefinition, tree1.root);
+    tree1.root->children.push_back(child1);
+    ASSERT_EQ(tree, tree1);
+}
+
+TEST(SyntaxTree, can_compare_different_syntax_tree) {
+    SyntaxTree tree, tree1;
+    tree.root = std::make_shared<Node>(NodeType::ProgramRoot);
+    tree1.root = std::make_shared<Node>(NodeType::ProgramRoot);
+    Node::Ptr child = std::make_shared<Node>(NodeType::FunctionDefinition, tree.root);
+    tree.root->children.push_back(child);
+    Node::Ptr child1 = std::make_shared<Node>(NodeType::FunctionReturnType, tree1.root);
+    tree1.root->children.push_back(child1);
+    ASSERT_NE(tree, tree1);
+}
