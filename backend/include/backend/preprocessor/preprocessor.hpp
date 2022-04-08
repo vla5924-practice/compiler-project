20730ce73e5c4c5b1ee6596f26bf49795fb1ce3e
vla5924 <vla5924@gmail.com>
2022-04-08 12:24:19 +0300

    updated preprocessor implementation with new utils::source

--- backend/include/backend/preprocessor/preprocessor.hpp
+++ backend/include/backend/preprocessor/preprocessor.hpp
@@ -1,18 +1,18 @@
 #pragma once
 
+#include <utils/source.hpp>
 #include "stringvec.hpp"
 
 namespace preprocessor {
 
 class Preprocessor {
-    static StringVec removeComments(const StringVec &source);
-
   public:
     Preprocessor() = delete;
     Preprocessor(const Preprocessor &) = delete;
     Preprocessor(Preprocessor &&) = delete;
     ~Preprocessor() = delete;
 
+    static void process(utils::Source &source);
     static StringVec process(const StringVec &source);
 };
 
--- backend/src/preprocessor/preprocessor.cpp
+++ backend/src/preprocessor/preprocessor.cpp
@@ -2,7 +2,41 @@
 
 using namespace preprocessor;
 
-StringVec Preprocessor::removeComments(const StringVec &source) {
+static void removeComments(utils::Source &source) {
+    auto &lineIter = source.lines.begin();
+    while (lineIter != source.lines.end()) {
+        bool inStringApostrophe = false;
+        bool inStringQuotes = false;
+        size_t i = 0;
+        std::string &str = lineIter->text;
+        for (; i < str.length(); i++) {
+            if (str[i] == '\'')
+                inStringApostrophe = !inStringApostrophe;
+            if (str[i] == '"')
+                inStringQuotes = !inStringQuotes;
+            if (!inStringApostrophe && !inStringQuotes) {
+                if (str[i] == '#') {
+                    break;
+                }
+            }
+        }
+        if (i != 0) {
+            str = str.substr(0, i);
+            lineIter++;
+        } else {
+            auto eraseIter = lineIter;
+            lineIter++;
+            source.lines.erase(eraseIter);
+        }
+    }
+}
+
+void Preprocessor::process(utils::Source &source) {
+    removeComments(source);
+}
+
+// old implementation
+StringVec Preprocessor::process(const StringVec &source) {
     StringVec result;
     for (const auto &str : source) {
         bool inStringApostrophe = false;
@@ -23,8 +57,4 @@ StringVec Preprocessor::removeComments(const StringVec &source) {
             result.emplace_back(std::move(str.substr(0, i)));
     }
     return result;
-}
-
-StringVec Preprocessor::process(const StringVec &source) {
-    return removeComments(source);
 }
