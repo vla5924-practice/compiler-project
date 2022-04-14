8e3f9d111c3ac0897389a1ae82b6983d8a0dab06
arteboss <43129320+arteboss@users.noreply.github.com>
2022-04-14 16:33:32 +0300

    added compile-time type conversion

--- backend/src/optimizer/optimizer.cpp
+++ backend/src/optimizer/optimizer.cpp
@@ -1,13 +1,31 @@
 #include "optimizer/optimizer.hpp"
 
-using namespace optimizer;
 using namespace ast;
+using namespace optimizer;
+
+Node::Ptr &firstChild(Node::Ptr &node) {
+    return node->children.front();
+}
+
+Node::Ptr &lastChild(Node::Ptr &node) {
+    return node->children.back();
+}
+
+void processTypeConversion(Node::Ptr &node) {
+    Node::Ptr &last_child = lastChild(node);
+    if (last_child->type == NodeType::IntegerLiteralValue || last_child->type == NodeType::FloatingPointLiteralValue) {
+        if (firstChild(node)->typeId() == BuiltInTypes::FloatType) {
+            node->type = NodeType::FloatingPointLiteralValue;
+            node->value = static_cast<float>(last_child->intNum());
+        } else {
+            node->type = NodeType::IntegerLiteralValue;
+            node->value = static_cast<long int>(last_child->fpNum());
+        }
+        node->children.clear();
+    }
+}
 
 void Optimizer::process(SyntaxTree &tree) {
     for (auto &node : tree.root->children) {
-        if (node->type == NodeType::FunctionDefinition) {
-            auto child = node->children.begin();
-            std::advance(child, 3);
-        }
     }
 }
