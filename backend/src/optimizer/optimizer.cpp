5aad436c34dbb94d12ce5c3ada75cebb0f6f327c
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2022-04-24 14:20:44 +0300

    integer constant folding

--- backend/src/optimizer/optimizer.cpp
+++ backend/src/optimizer/optimizer.cpp
@@ -25,7 +25,76 @@ void processTypeConversion(Node::Ptr &node) {
     }
 }
 
+void constantFolding(Node::Ptr &first, Node::Ptr &second) {
+    if (first->type == NodeType::IntegerLiteralValue && second->type == NodeType::IntegerLiteralValue) {
+        long int result = 0;
+        switch (first->parent->binOp()) {
+        case BinaryOperation::Add:
+            result = first->intNum() + second->intNum();
+            break;
+        case BinaryOperation::Sub:
+            result = first->intNum() - second->intNum();
+            break;
+        case BinaryOperation::Mult:
+            result = first->intNum() * second->intNum();
+            break;
+        case BinaryOperation::Div:
+            result = first->intNum() / second->intNum();
+            break;
+        default:
+            return;
+            break;
+        }
+        auto parent = first->parent;
+        parent->type = NodeType::IntegerLiteralValue;
+        parent->value = result;
+        parent->children.clear();
+    }
+}
+
+void processExpression(Node::Ptr &node, std::list<VariablesTable *> &table) {
+    for (auto &child : node->children) {
+        if (child->type == NodeType::TypeConversion) {
+            processTypeConversion(child);
+        }
+    }
+
+    for (auto &child : node->children) {
+        if (child->type == NodeType::BinaryOperation) {
+            auto first = firstChild(child);
+            auto second = lastChild(child);
+            if (first->type == NodeType::TypeConversion)
+                processTypeConversion(first);
+            if (second->type == NodeType::TypeConversion)
+                processTypeConversion(second);
+            constantFolding(first, second);
+            return;
+        }
+
+        processExpression(child, table);
+    }
+}
+
+void processBranchRoot(Node::Ptr &node, std::list<VariablesTable *> &table) {
+    table.push_front(&node->variables());
+    for (auto &child : node->children) {
+        if (child->type == NodeType::Expression) {
+            processExpression(node, table);
+        }
+
+        if (child->type == NodeType::VariableDeclaration) {
+            processExpression(node, table);
+        }
+    }
+}
+
 void Optimizer::process(SyntaxTree &tree) {
     for (auto &node : tree.root->children) {
+        if (node->type == NodeType::FunctionDefinition) {
+            auto child = node->children.begin();
+            std::advance(child, 3);
+            std::list<VariablesTable *> variables_table;
+            processBranchRoot(*child, variables_table);
+        }
     }
 }
