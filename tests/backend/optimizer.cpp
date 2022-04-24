ef1d5ca480a36538abc0eec803dd338e1ebdafbe
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2022-04-24 19:01:04 +0300

    style

--- tests/backend/optimizer.cpp
+++ tests/backend/optimizer.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 
 #include "lexer/lexer.hpp"
+#include "optimizer/optimizer.hpp"
 #include "parser/parser.hpp"
 #include "semantizer/semantizer.hpp"
-#include "optimizer/optimizer.hpp"
+
 
 using namespace ast;
 using namespace lexer;
@@ -12,15 +13,12 @@ using namespace semantizer;
 using namespace optimizer;
 
 TEST(Optimaizer, can_fill_functions_table) {
-    StringVec source = {"def main() -> None:", 
-    "    x: float = 1 + 1.0", 
-    "    y: float = 1", 
-    "    y = 1.0 - x"};
+    StringVec source = {"def main() -> None:", "    x: float = 1 + 1.0", "    y: float = 1", "    y = 1.0 - x"};
     TokenList token_list = Lexer::process(source);
     SyntaxTree tree = Parser::process(token_list);
     Semantizer::process(tree);
     tree.dump(std::cout);
     Optimizer::process(tree);
     tree.dump(std::cout);
-    ASSERT_EQ(2, 1+1);
+    ASSERT_EQ(2, 1 + 1);
 }
