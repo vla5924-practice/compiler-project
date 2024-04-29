3ed9404d3978bac4db41df392b3c4f1fca6b2d59
vla5924 <vla5924@gmail.com>
2024-04-29 15:20:32 +0300

    remove init

--- compiler/include/compiler/optree/adaptors.hpp
+++ compiler/include/compiler/optree/adaptors.hpp
@@ -2,7 +2,6 @@
 
 #include <cstdint>
 #include <string>
-#include <type_traits>
 #include <vector>
 
 #include "compiler/optree/base_adaptor.hpp"
@@ -76,18 +75,6 @@ struct ConstantOp : Adaptor {
     void init(const Type::Ptr &type, bool value);
     void init(const Type::Ptr &type, double value);
     void init(const Type::Ptr &type, const std::string &value);
-
-    template <typename T>
-    void init(const Type::Ptr &type, const T &value) {
-        if constexpr (std::is_same_v<std::remove_cvref_t<T>, bool>)
-            init(type, value);
-        else if constexpr (std::is_integral_v<T>)
-            init(type, static_cast<int64_t>(value));
-        else if constexpr (std::is_floating_point_v<T>)
-            init(type, static_cast<double>(value));
-        else
-            init(type, value);
-    }
 
     OPTREE_ADAPTOR_ATTRIBUTE_OPAQUE(value, 0)
     OPTREE_ADAPTOR_RESULT(result, 0)
