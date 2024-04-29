7a812d7990382c9ff0751a2e2d803a86cc572aa6
Artem <43129320+arteboss@users.noreply.github.com>
2024-04-29 14:52:01 +0000

    Fold constants

--- compiler/lib/backend/optree/optimizer/transforms/fold_constants.cpp
+++ compiler/lib/backend/optree/optimizer/transforms/fold_constants.cpp
@@ -33,22 +33,208 @@ struct FoldConstants : public Transform<ArithBinaryOp, ArithCastOp, LogicBinaryO
             case ArithBinOpKind::AddI:
                 folded = lhs + rhs;
                 break;
+            case ArithBinOpKind::SubI:
+                folded = lhs - rhs;
+                break;
             case ArithBinOpKind::MulI:
                 folded = lhs * rhs;
                 break;
+            case ArithBinOpKind::DivI:
+                folded = lhs / rhs;
+                break;
             default:
-                folded = -1; // TODO: extend
+                folded = -1;
+            }
+            auto newOp = builder.insert<ConstantOp>(op->ref, type, folded);
+            builder.replace(op, newOp);
+            return;
+        } else if (type->is<FloatType>()) {
+            double folded = 0;
+            double lhs = lhsOp.value().as<double>();
+            double rhs = rhsOp.value().as<double>();
+            switch (op.kind()) {
+            case ArithBinOpKind::AddF:
+                folded = lhs + rhs;
+                break;
+            case ArithBinOpKind::SubF:
+                folded = lhs - rhs;
+                break;
+            case ArithBinOpKind::MulF:
+                folded = lhs * rhs;
+                break;
+            case ArithBinOpKind::DivF:
+                folded = lhs / rhs;
+                break;
+            default:
+                folded = -1;
             }
             auto newOp = builder.insert<ConstantOp>(op->ref, type, folded);
             builder.replace(op, newOp);
             return;
         }
-        // TODO: support other types
+    }
+
+    static void foldArithCastOp(const ArithCastOp &op, OptBuilder &builder) {
+        auto valueOp = getValueOwnerAs<ArithCastOp>(op.value());
+        if (!valueOp)
+            return;
+        auto valueType = op.value()->type;
+        auto type = op.result()->type;
+        if (type->is<IntegerType>()) {
+            int64_t folded = 0;
+            if (valueType->is<IntegerType>()) {
+                int64_t value = valueOp.value().as<int64_t>();
+                switch (op.kind()) {
+                case ArithCastOpKind::ExtI:
+                case ArithCastOpKind::TruncI:
+                    folded = value;
+                    break;
+                default:
+                    folded = -1;
+                }
+            } else if (valueType->is<FloatType>()) {
+                double value = valueOp.value().as<double>();
+                switch (op.kind()) {
+                case ArithCastOpKind::FloatToInt:
+                    folded = static_cast<int64_t>(value);
+                    break;
+                default:
+                    folded = -1;
+                }
+            }
+            auto newOp = builder.insert<ConstantOp>(op->ref, type, folded);
+            builder.replace(op, newOp);
+            return;
+        } else if (type->is<FloatType>()) {
+            double folded = 0;
+            if (valueType->is<FloatType>()) {
+                double value = valueOp.value().as<double>();
+                switch (op.kind()) {
+                case ArithCastOpKind::ExtF:
+                case ArithCastOpKind::TruncF:
+                    folded = value;
+                    break;
+                default:
+                    folded = -1;
+                }
+            } else if (valueType->is<IntegerType>()) {
+                double value = valueOp.value().as<double>();
+                switch (op.kind()) {
+                case ArithCastOpKind::IntToFloat:
+                    folded = static_cast<double>(value);
+                    break;
+                default:
+                    folded = -1;
+                }
+            }
+            auto newOp = builder.insert<ConstantOp>(op->ref, type, folded);
+            builder.replace(op, newOp);
+            return;
+        }
+    }
+
+    static void foldLogicBinaryOp(const LogicBinaryOp &op, OptBuilder &builder) {
+        auto lhsOp = getValueOwnerAs<ConstantOp>(op.lhs());
+        auto rhsOp = getValueOwnerAs<ConstantOp>(op.rhs());
+        if (!lhsOp || !rhsOp)
+            return;
+        auto type = op.result()->type;
+        auto valType = op.lhs()->type;
+        bool folded = 0;
+        if (valType->is<IntegerType>()) {
+            int64_t lhs = lhsOp.value().as<int64_t>();
+            int64_t rhs = rhsOp.value().as<int64_t>();
+            switch (op.kind()) {
+            case LogicBinOpKind::Equal:
+                folded = lhs == rhs;
+                break;
+            case LogicBinOpKind::NotEqual:
+                folded = lhs != rhs;
+                break;
+            case LogicBinOpKind::LessEqualI:
+                folded = lhs <= rhs;
+                break;
+            case LogicBinOpKind::LessI:
+                folded = lhs < rhs;
+                break;
+            case LogicBinOpKind::GreaterEqualI:
+                folded = lhs >= rhs;
+                break;
+            case LogicBinOpKind::GreaterI:
+                folded = lhs > rhs;
+                break;
+            default:
+                folded = -1;
+            }
+        } else if (valType->is<FloatType>()) {
+            double lhs = lhsOp.value().as<double>();
+            double rhs = rhsOp.value().as<double>();
+            switch (op.kind()) {
+            case LogicBinOpKind::Equal:
+                folded = lhs == rhs;
+                break;
+            case LogicBinOpKind::NotEqual:
+                folded = lhs != rhs;
+                break;
+            case LogicBinOpKind::LessEqualF:
+                folded = lhs <= rhs;
+                break;
+            case LogicBinOpKind::LessF:
+                folded = lhs < rhs;
+                break;
+            case LogicBinOpKind::GreaterEqualF:
+                folded = lhs >= rhs;
+                break;
+            case LogicBinOpKind::GreaterF:
+                folded = lhs > rhs;
+                break;
+            default:
+                folded = -1;
+            }
+        } else if (valType->is<BoolType>()) {
+            bool lhs = lhsOp.value().as<bool>();
+            bool rhs = rhsOp.value().as<bool>();
+            switch (op.kind()) {
+            case LogicBinOpKind::AndI:
+                folded = lhs && rhs;
+                break;
+            case LogicBinOpKind::OrI:
+                folded = lhs || rhs;
+                break;
+            default:
+                folded = -1;
+            }
+        }
+        auto newOp = builder.insert<ConstantOp>(op->ref, type, folded);
+        builder.replace(op, newOp);
+        return;
+    }
+
+    static void foldLogicUnaryOp(const LogicUnaryOp &op, OptBuilder &builder) {
+        auto valueOp = getValueOwnerAs<ArithCastOp>(op.value());
+        if (!valueOp)
+            return;
+        auto type = op.result()->type;
+        bool folded = 0;
+        bool value = valueOp.value().as<bool>();
+        switch (op.kind()) {
+        case LogicUnaryOpKind::Not:
+            folded = !value;
+            break;
+        default:
+            folded = -1;
+        }
     }
 
     void run(const Operation::Ptr &op, OptBuilder &builder) const override {
         if (op->is<ArithBinaryOp>())
             foldArithBinaryOp(op->as<ArithBinaryOp>(), builder);
+        else if (op->is<ArithCastOp>())
+            foldArithCastOp(op->as<ArithCastOp>(), builder);
+        else if (op->is<LogicBinaryOp>())
+            foldLogicBinaryOp(op->as<LogicBinaryOp>(), builder);
+        else if (op->is<LogicUnaryOp>())
+            foldLogicUnaryOp(op->as<LogicUnaryOp>(), builder);
     }
 };
 
