da740317a79bffe18c3bb2a20a3a1fbc627fd10e
vla5924 <vla5924@gmail.com>
2024-05-11 11:22:12 +0300

    language header

---
+++ compiler/include/compiler/utils/language.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+namespace utils {
+namespace language {
+
+// ----------------------------------------------------------------------------
+// Reserved function names
+// ----------------------------------------------------------------------------
+
+inline const std::string funcMain = "main";
+inline const std::string funcPrint = "print";
+inline const std::string funcInput = "input";
+inline const std::string funcRange = "range";
+
+// ----------------------------------------------------------------------------
+// Reserved decorator names
+// ----------------------------------------------------------------------------
+
+inline const std::string atInline = "inline";
+
+} // namespace language
+} // namespace utils
