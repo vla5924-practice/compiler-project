29d4c65247485db911c4cc70806b8c2088497cbf
Maksim Vlasov <vla5924@gmail.com>
2024-05-25 11:28:04 +0300

    Support optree compilation backend in CLI (#169)
    * Refactor executor as a stage runner
    * Extract command line arguments parsing into separate module
    * Add Git-based versioning
    
    Update argparse revision to f0759fd
    https://github.com/p-ranav/argparse/commit/f0759fd982bb4a88785094626ea522cb3a84ec84

--- compiler/lib/cli/main.cpp
+++ compiler/lib/cli/main.cpp
@@ -1,5 +1,30 @@
+#include <iostream>
+#include <utility>
+
 #include "compiler.hpp"
+#include "options.hpp"
+
+using namespace cli;
 
 int main(int argc, char *argv[]) {
-    return Compiler::exec(argc, argv);
+    Options opt;
+
+    try {
+        opt = std::move(parseArguments(argc, argv));
+    } catch (const OptionsError &err) {
+        std::cerr << err.what() << "\n";
+        return 1;
+    }
+
+    Compiler compiler(opt);
+    int ret = compiler.run();
+    if (ret)
+        return ret;
+
+    if (opt.time) {
+        for (const auto &[stage, elapsed] : compiler.measurements())
+            std::cerr << stage << " Elapsed time: " << elapsed << " ms\n";
+    }
+
+    return 0;
 }
