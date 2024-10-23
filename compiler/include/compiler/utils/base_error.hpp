d0b65c773124ef42a2d9e9fe4ca23f95512b542c
Maksim Vlasov <vla5924@gmail.com>
2024-10-23 16:27:45 +0300

    Append values to error message via operator<< (#186)
    Diagnostic message of BaseError class (and its derivatives) instances
    can be appended with stream-like syntax. To conveniently utilize this
    feature, ErrorBuffer::push method now returns a pointer to new instance.

--- compiler/include/compiler/utils/base_error.hpp
+++ compiler/include/compiler/utils/base_error.hpp
@@ -6,12 +6,43 @@
 #include "compiler/utils/source_ref.hpp"
 
 class BaseError : public std::exception {
-    std::string what_str;
+    std::string message;
 
   public:
-    BaseError(const utils::SourceRef &ref, const std::string &message);
-    BaseError(const std::string &message);
+    BaseError(const BaseError &) = default;
+    BaseError(BaseError &&) = default;
     ~BaseError() = default;
 
+    explicit BaseError(const utils::SourceRef &ref, const std::string &initMessage = {});
+    explicit BaseError(const std::string &initMessage = {});
+
     virtual const char *what() const noexcept;
+
+    template <typename T>
+        requires std::same_as<decltype(message + std::declval<T>()), std::string>
+    BaseError &operator<<(const T &value) {
+        message += value;
+        return *this;
+    }
+
+    template <typename T>
+        requires std::same_as<decltype(message + std::to_string(std::declval<T>())), std::string>
+    BaseError &operator<<(const T &value) {
+        message += std::to_string(value);
+        return *this;
+    }
+
+    template <typename T>
+        requires std::same_as<decltype(std::declval<T>().dump()), std::string>
+    BaseError &operator<<(const T &value) {
+        message += value.dump();
+        return *this;
+    }
+
+    template <typename T>
+        requires std::same_as<decltype(std::declval<T>()->dump()), std::string>
+    BaseError &operator<<(const T &value) {
+        message += value->dump();
+        return *this;
+    }
 };
