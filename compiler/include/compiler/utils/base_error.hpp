00db803268616a79c0ce9080b0b2ed97f5a8a8e8
Maksim Vlasov <vla5924@gmail.com>
2024-10-23 17:25:48 +0300

    Allow Type objects to be dumped via BaseError operator<< (#187)
    `Type` class interface was lacking `std::string dump() const` method.
    Also, to resolve overload ambiguity in favor of `std::to_string`
    approach for dumping integers, an additional constraint was added to
    `BaseError::operator<<` declaration.

--- compiler/include/compiler/utils/base_error.hpp
+++ compiler/include/compiler/utils/base_error.hpp
@@ -19,15 +19,15 @@ class BaseError : public std::exception {
     virtual const char *what() const noexcept;
 
     template <typename T>
-        requires std::same_as<decltype(message + std::declval<T>()), std::string>
+        requires std::same_as<decltype(message += std::declval<T>()), std::string &> && (!std::integral<T>)
     BaseError &operator<<(const T &value) {
         message += value;
         return *this;
     }
 
     template <typename T>
-        requires std::same_as<decltype(message + std::to_string(std::declval<T>())), std::string>
-    BaseError &operator<<(const T &value) {
+        requires std::same_as<decltype(std::to_string(std::declval<T>())), std::string>
+    BaseError &operator<<(T value) {
         message += std::to_string(value);
         return *this;
     }
