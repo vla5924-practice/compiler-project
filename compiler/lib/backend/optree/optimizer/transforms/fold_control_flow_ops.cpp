337a1b185136e097d40ae5f72aeca3a4e2442621
Artem <43129320+arteboss@users.noreply.github.com>
2025-03-15 15:07:46 +0000

    Implement fold control flow optimization

---
+++ compiler/lib/backend/optree/optimizer/transforms/fold_control_flow_ops.cpp
@@ -0,0 +1,77 @@
+#include "optimizer/transform.hpp"
+
+#include <memory>
+#include <iostream>
+
+#include "compiler/optree/adaptors.hpp"
+#include "compiler/optree/helpers.hpp"
+#include "compiler/optree/operation.hpp"
+
+#include "optimizer/opt_builder.hpp"
+
+using namespace optree;
+using namespace optree::optimizer;
+
+namespace {
+
+struct FoldControlFlowOps : public Transform<IfOp, WhileOp> {
+    using Transform::Transform;
+    
+    std::string_view name() const override {
+        return "FoldControlFlowOps";
+    }
+
+    static void hoistBody(const Operation::Ptr &op, OptBuilder &builder) {
+        if (!op)
+            return;
+        auto& parent = op->parent;
+        builder.setInsertPointBefore(parent);
+        for (const auto &childOp : utils::advanceEarly(op->body)) {
+            auto cloned = builder.clone(childOp);
+            builder.replace(childOp, cloned); 
+            builder.setInsertPointAfter(cloned);
+        }
+    }
+
+    static void processIfOp(const IfOp &op, OptBuilder &builder) {
+        auto conditionOp = getValueOwnerAs<ConstantOp>(op.cond());
+        if (!conditionOp)
+            return;
+        bool condition = conditionOp.value().as<bool>();
+        if (condition) {
+            hoistBody(op.thenOp(), builder);
+        } else {
+            hoistBody(op.elseOp(), builder);
+        }        
+        builder.erase(op);
+    }
+
+    static void processWhileOp(const WhileOp &op, OptBuilder &builder) {
+        auto conditionOp = getValueOwnerAs<ConstantOp>(op.conditionOp().terminator());
+        if (!conditionOp)
+            return;
+        bool condition = conditionOp.value().as<bool>();
+        if (!condition) {
+            builder.erase(op);
+        }
+    }
+
+    void run(const Operation::Ptr &op, OptBuilder &builder) const override {
+        if (auto ifOp = op->as<IfOp>())
+            processIfOp(ifOp, builder);
+        else if (auto whileOp = op->as<WhileOp>())
+            processWhileOp(whileOp, builder);
+    }
+};
+
+} // namespace
+
+namespace optree {
+namespace optimizer {
+
+BaseTransform::Ptr createFoldControlFlowOps() {
+    return std::make_shared<FoldControlFlowOps>();
+}
+
+} // namespace optimizer
+} // namespace optree
---
+++ compiler/tests/backend/optree/optimizer/fold_control_flow_ops.cpp
@@ -0,0 +1,59 @@
+#include <gtest/gtest.h>
+
+#include <utility>
+#include <iostream>
+
+#include "compiler/backend/optree/optimizer/optimizer.hpp"
+#include "compiler/backend/optree/optimizer/transform_factories.hpp"
+#include "compiler/optree/adaptors.hpp"
+
+#include "common.hpp"
+
+using namespace optree;
+using namespace optree::optimizer;
+
+class FoldControlFlowTest : public TransformTestBase {
+    virtual void setupOptimizer(Optimizer &opt) const override {
+        opt.add(createFoldControlFlowOps());
+    }
+
+  public:
+  FoldControlFlowTest() = default;
+    ~FoldControlFlowTest() = default;
+};
+
+TEST_F(FoldControlFlowTest, can_run_on_empty_optree) {
+    runOptimizer();
+    assertSameOpTree();
+}
+
+TEST_F(FoldControlFlowTest, can_fold_control_flow_ops) {
+    {
+        auto &&[m, v] = getActual();
+        m.opInit<FunctionOp>("test", m.tFunc({m.tI64, m.tF64}, m.tNone)).inward(v["x"], 0).inward(v["y"], 1).withBody();
+        v[0] = m.opInit<ConstantOp>(m.tBool, true);
+        m.op<IfOp>(v[0]).withBody();
+        m.op<ThenOp>().withBody();
+        v[1] = m.opInit<ConstantOp>(m.tF64, 2.3);
+        m.opInit<ArithBinaryOp>(ArithBinOpKind::AddF, v["y"], v[1]);
+        m.endBody();
+        m.op<ElseOp>().withBody();
+        v[2] = m.opInit<ConstantOp>(m.tI64, int64_t(2));
+        m.opInit<ArithBinaryOp>(ArithBinOpKind::AddI, v["x"], v[2]);
+        m.endBody();
+        m.endBody();
+        m.opInit<ReturnOp>();
+        m.endBody();
+    }
+    {
+        auto &&[m, v] = getExpected();
+        m.opInit<FunctionOp>("test", m.tFunc({m.tI64, m.tF64}, m.tNone)).inward(v["x"], 0).inward(v["y"], 1).withBody();
+        v[0] = m.opInit<ConstantOp>(m.tBool, true);
+        v[1] = m.opInit<ConstantOp>(m.tF64, 2.3);
+        m.opInit<ArithBinaryOp>(ArithBinOpKind::AddF, v["y"], v[1]);
+        m.opInit<ReturnOp>();
+        m.endBody();
+    }
+    runOptimizer();
+    assertSameOpTree();
+}
