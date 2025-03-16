03816083386db28dd24311ad4998ebd0a6288053
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2025-03-16 13:33:13 +0300

    classic boolean reduction rules

--- compiler/lib/backend/optree/optimizer/transforms/boolean_expression_minimization.cpp
+++ compiler/lib/backend/optree/optimizer/transforms/boolean_expression_minimization.cpp
@@ -1,11 +1,8 @@
-#include <deque>
-#include <map>
-#include <memory>
 #include <string>
 #include <string_view>
-#include <unordered_set>
 
 #include "compiler/optree/adaptors.hpp"
+#include "compiler/optree/helpers.hpp"
 #include "compiler/optree/operation.hpp"
 #include "compiler/utils/helpers.hpp"
 #include "compiler/utils/language.hpp"
@@ -18,15 +15,107 @@ using namespace optree::optimizer;
 
 namespace {
 
-struct BooleanExpressionMinimization : public Transform<ModuleOp> {
+struct BooleanExpressionMinimization : public Transform<LogicBinaryOp> {
     using Transform::Transform;
 
     std::string_view name() const override {
         return "BooleanExpressionMinimization";
     }
 
+    // x ∨ x = x, x ∧ x = x
+    static bool checkIdempotence(const LogicBinaryOp &logicOp) {
+        auto lhs = logicOp.lhs()->owner.lock();
+        auto rhs = logicOp.rhs()->owner.lock();
+        return similar(lhs, rhs);
+    }
+
+    static bool proccesOperand(const ConstantOp &constOp, const Operation::Ptr &secondOp, bool annihilatorValue,
+                               OptBuilder &builder) {
+        auto valueType = constOp->result(0)->type;
+        auto &parent = constOp->parent;
+        auto replaceFunc = [&parent, &secondOp, annihilatorValue]<typename T>(T value) {
+            bool opSwitch = annihilatorValue ? !value : value;
+            if (opSwitch) {
+                builder.replace(parent, secondOp);
+            } else {
+                auto newOp = builder.insert<ConstantOp>(parent->ref, TypeStorage::boolType(), annihilatorValue);
+                builder.replace(parent, newOp);
+            }
+        };
+        if (valueType->is<BoolType>()) {
+            auto value = constOp.value().as<NativeBool>();
+            replaceFunc(value);
+            return;
+        }
+        if (valueType->is<IntegerType>()) {
+            auto value = constOp.value().as<NativeInt>();
+            replaceFunc(value);
+            return;
+        }
+        if (valueType->is<FloatType>()) {
+            auto value = constOp.value().as<NativeFloat>();
+            replaceFunc(value);
+            return;
+        }
+    }
+
+    static void proccesIdentityAndAnnihilatorRules(const LogicBinaryOp &logicOp, bool annihilatorValue,
+                                                   OptBuilder &builder) {
+        auto constLhsOp = getValueOwnerAs<ConstantOp>(logicOp.lhs());
+        bool processed = false;
+        if (constLhsOp) {
+            processed = proccesOperand(constLhsOp, logicOp.rhs()->owner.lock(), annihilatorValue, builder);
+        }
+        auto constRhsOp = getValueOwnerAs<ConstantOp>(logicOp.rhs());
+        if (constRhsOp && !processed) {
+            proccesOperand(constRhsOp, logicOp.lhs()->owner.lock(), annihilatorValue, builder);
+        }
+    }
+
+    static void proccesAnd(const LogicBinaryOp &logicOp, OptBuilder &builder) {
+        if (checkIdempotence(logicOp)) {
+            builder.replace(logicOp, logicOp.lhs()->owner.lock());
+        } else {
+            proccesIdentityAndAnnihilatorRules(logicOp, false, builder);
+        }
+    }
+
+    static void proccesOr(const LogicBinaryOp &logicOp, OptBuilder &builder) {
+        if (checkIdempotence(logicOp)) {
+            builder.replace(logicOp, logicOp.lhs()->owner.lock());
+        } else {
+            proccesIdentityAndAnnihilatorRules(logicOp, true, builder);
+        }
+    }
+
+    static void proccesEqual(const LogicBinaryOp &logicOp, OptBuilder &builder) {
+        if (checkIdempotence(logicOp)) {
+            auto newOp = builder.insert<ConstantOp>(logicOp->ref, TypeStorage::boolType(), true);
+            builder.replace(logicOp, newOp);
+        }
+    }
+
+    static void proccesNotEqual(const LogicBinaryOp &logicOp, OptBuilder &builder) {
+        if (checkIdempotence(logicOp)) {
+            auto newOp = builder.insert<ConstantOp>(logicOp->ref, TypeStorage::boolType(), false);
+            builder.replace(logicOp, newOp);
+        }
+    }
+
     void run(const Operation::Ptr &op, OptBuilder &builder) const override {
-        
+        auto logicOp = op->as<LogicBinaryOp>();
+        if (logicOp.kind() == LogicBinOpKind::Equal) {
+            proccesEqual(logicOp, builder);
+        }
+        if (logicOp.kind() == LogicBinOpKind::NotEqual) {
+            proccesNotEqual(logicOp, builder);
+        }
+        if (logicOp.kind() == LogicBinOpKind::AndI) {
+            proccesAnd(logicOp, builder);
+        }
+        if (logicOp.kind() == LogicBinOpKind::OrI) {
+            proccesOr(logicOp, builder);
+        }
     }
 };
 
--- compiler/lib/backend/optree/optimizer/transforms/ordering_commutativity_ops.cpp
+++ compiler/lib/backend/optree/optimizer/transforms/ordering_commutativity_ops.cpp
@@ -24,56 +24,66 @@ struct OrderingCommutativityOps : public Transform<ArithBinaryOp, LogicBinaryOp>
     std::string_view name() const override {
         return "OrderingCommutativityOps";
     }
+    // clang-format off
+    static inline std::array<ArithBinOpKind, 4> commutativityArithOps{
+        ArithBinOpKind::AddI,
+        ArithBinOpKind::AddF,
+        ArithBinOpKind::MulI,
+        ArithBinOpKind::MulF
+    };
 
-    static inline std::array<ArithBinOpKind, 4> commutativityBinaryOps{ArithBinOpKind::AddI, ArithBinOpKind::AddF,
-                                                                       ArithBinOpKind::MulI, ArithBinOpKind::MulF};
-
-    static void sortArithBinaryOprands(const ArithBinaryOp &op, OptBuilder &builder) {
-        if (std::find(commutativityBinaryOps.cbegin(), commutativityBinaryOps.cend(), op.kind()) ==
-            commutativityBinaryOps.end()) {
-            return;
-        }
+    static inline std::array<LogicBinOpKind, 4> commutativityLogicOps{
+        LogicBinOpKind::AndI,
+        LogicBinOpKind::Equal,
+        LogicBinOpKind::OrI,
+        LogicBinOpKind::NotEqual
+    };
+    // clang-format on
+    static void sortBinaryOprands(const BinaryOp &op, OptBuilder &builder) {
         if (auto lhs = getValueOwnerAs<ConstantOp>(op.lhs())) {
-            if (auto rhs = getValueOwnerAs<ConstantOp>(op.rhs())) {
+            if (auto rhs = getValueOwnerAs<ConstantOp>(op.rhs()))
                 if (lhs->attributes[0].storage > rhs->attributes[0].storage)
-                    std::swap(op->operands[0], op->operands[1]);
-            }
-            if (auto rhs = getValueOwnerAs<LoadOp>(op.rhs())) {
-                std::swap(op->operands[0], op->operands[1]);
-            }
+                    builder.update(op, [&op, &lhs, &rhs]() { std::swap(op->operands[0], op->operands[1]); });
+            if (auto rhs = getValueOwnerAs<LoadOp>(op.rhs()))
+                builder.update(op, [&op, &lhs, &rhs]() { std::swap(op->operands[0], op->operands[1]); });
         }
         if (auto lhs = getValueOwnerAs<LoadOp>(op.lhs())) {
-            if (auto rhs = getValueOwnerAs<LoadOp>(op.rhs())) {
+            if (auto rhs = getValueOwnerAs<LoadOp>(op.rhs()))
                 if (lhs > rhs)
-                    std::swap(op->operands[0], op->operands[1]);
-            }
+                    builder.update(op, [&op, &lhs, &rhs]() { std::swap(op->operands[0], op->operands[1]); });
         }
         if (auto lhs = getValueOwnerAs<FunctionCallOp>(op.lhs())) {
-            if (auto rhs = getValueOwnerAs<ConstantOp>(op.rhs())) {
-                std::swap(op->operands[0], op->operands[1]);
-            }
-            if (auto rhs = getValueOwnerAs<LoadOp>(op.rhs())) {
-                std::swap(op->operands[0], op->operands[1]);
-            }
+            if (auto rhs = getValueOwnerAs<ConstantOp>(op.rhs()))
+                builder.update(op, [&op, &lhs, &rhs]() { std::swap(op->operands[0], op->operands[1]); });
+            if (auto rhs = getValueOwnerAs<LoadOp>(op.rhs()))
+                builder.update(op, [&op, &lhs, &rhs]() { std::swap(op->operands[0], op->operands[1]); });
             if (auto rhs = getValueOwnerAs<FunctionCallOp>(op.rhs())) {
                 if (std::lexicographical_compare(lhs->name.begin(), lhs->name.end(), rhs->name.begin(),
-                                                 rhs->name.end())) {
-                    std::swap(op->operands[0], op->operands[1]);
-                } else {
+                                                 rhs->name.end()))
+                    builder.update(op, [&op, &lhs, &rhs]() { std::swap(op->operands[0], op->operands[1]); });
+                else {
                     // TODO check function argument order
                 }
             }
         }
     }
 
-    static void sortLogicBinaryOprands(const LogicBinaryOp &op, OptBuilder &builder) {
-    }
-
     void run(const Operation::Ptr &op, OptBuilder &builder) const override {
-        if (op->is<ArithBinaryOp>())
-            sortArithBinaryOprands(op->as<ArithBinaryOp>(), builder);
-        else if (op->is<LogicBinaryOp>())
-            sortLogicBinaryOprands(op->as<LogicBinaryOp>(), builder);
+        if (op->is<ArithBinaryOp>()) {
+            auto arithOp = op->as<ArithBinaryOp>();
+            if (std::find(commutativityArithOps.cbegin(), commutativityArithOps.cend(), arithOp.kind()) ==
+                commutativityArithOps.end()) {
+                return;
+            }
+            sortBinaryOprands(op->as<BinaryOp>(), builder);
+        } else if (op->is<LogicBinaryOp>()) {
+            auto logicOp = op->as<LogicBinaryOp>();
+            if (std::find(commutativityLogicOps.cbegin(), commutativityLogicOps.cend(), logicOp.kind()) ==
+                commutativityLogicOps.end()) {
+                return;
+            }
+            sortBinaryOprands(op->as<BinaryOp>(), builder);
+        }
     }
 };
 
